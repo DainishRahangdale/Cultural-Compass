5458016314a494f411320b1e9de911cd69b913ab
Dainish Rahangdale <dainish74892002@gmail.com>
2025-01-12 16:29:35 +0530

    basic styling done

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const methodOverride = require('method-override')
 app.set('view engine','ejs');
 
 app.set('views', path.join(__dirname,'views'));
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(express.urlencoded({extended:true}))  // for parsing
 app.use(methodOverride('_method'));
